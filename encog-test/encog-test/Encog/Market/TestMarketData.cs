c8b1d6e15f6259c222a593ba05100af9984c3587
JeffHeatonDotCom <JeffHeatonDotCom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2009-06-22 10:24:01 +0000

    Added more unit tests

---
+++ encog-test/encog-test/Encog/Market/TestMarketData.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Encog.Neural.NeuralData.Market.Loader;
+using Encog.Neural.NeuralData.Market;
+using Encog.Neural.NeuralData.Temporal;
+using Encog.Neural.NeuralData;
+
+namespace encog_test.Data.Market
+{
+    [TestFixture]
+    public class TestMarketData
+    {
+        [Test]
+        public void MarketData()
+        {
+            IMarketLoader loader = new YahooFinanceLoader();
+            TickerSymbol tickerAAPL = new TickerSymbol("AAPL", null);
+            TickerSymbol tickerMSFT = new TickerSymbol("MSFT", null);
+            MarketNeuralDataSet marketData = new MarketNeuralDataSet(loader, 5, 1);
+            marketData.AddDescription(new MarketDataDescription(tickerAAPL, MarketDataType.CLOSE, true, true));
+            marketData.AddDescription(new MarketDataDescription(tickerMSFT, MarketDataType.CLOSE, true, false));
+            marketData.AddDescription(new MarketDataDescription(tickerAAPL, MarketDataType.VOLUME, true, false));
+            marketData.AddDescription(new MarketDataDescription(tickerMSFT, MarketDataType.VOLUME, true, false));
+            DateTime begin = new DateTime(2008, 7, 1);
+            DateTime end = new DateTime(2008, 7, 31);
+            marketData.Load(begin, end);
+            marketData.Generate();
+            Assert.AreEqual(22, marketData.Points.Count);
+
+            // first test the points
+            IEnumerator<TemporalPoint> itr = marketData.Points.GetEnumerator();
+            itr.MoveNext();
+            TemporalPoint point = itr.Current;
+            Assert.AreEqual(0, point.Sequence);
+            Assert.AreEqual(4, point.Data.Length);
+            Assert.AreEqual(174.68, point.Data[0]);
+            Assert.AreEqual(26.87, point.Data[1]);
+            Assert.AreEqual(39, (int)(point.Data[2] / 1000000));
+            Assert.AreEqual(100, (int)(point.Data[3] / 1000000));
+
+            itr.MoveNext();
+            point = itr.Current;
+            Assert.AreEqual(1, point.Sequence);
+            Assert.AreEqual(4, point.Data.Length);
+            Assert.AreEqual(168.18, point.Data[0]);
+            Assert.AreEqual(25.88, point.Data[1]);
+            Assert.AreEqual(29, (int)(point.Data[2] / 1000000));
+            Assert.AreEqual(84, (int)(point.Data[3] / 1000000));
+
+            itr.MoveNext();
+            point = itr.Current;
+            Assert.AreEqual(2, point.Sequence);
+            Assert.AreEqual(4, point.Data.Length);
+            Assert.AreEqual(170.12, point.Data[0]);
+            Assert.AreEqual(25.98, point.Data[1]);
+            Assert.AreEqual(18, (int)(point.Data[2] / 1000000));
+            Assert.AreEqual(37, (int)(point.Data[3] / 1000000));
+
+            // now check the actual data
+            Assert.AreEqual(16, marketData.Data.Count);
+            Assert.AreEqual(20, marketData.InputNeuronCount);
+            Assert.AreEqual(1, marketData.OutputNeuronCount);
+
+            IEnumerator<INeuralDataPair> itr2 = marketData.Data.GetEnumerator();
+            itr2.MoveNext();
+            INeuralDataPair pair = itr2.Current;
+            Assert.AreEqual(20, pair.Input.Count);
+            Assert.AreEqual(1, pair.Ideal.Count);
+
+
+            Assert.AreEqual(-0.037, Math.Round(pair.Input.Data[0] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.037, Math.Round(pair.Input.Data[1] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.246, Math.Round(pair.Input.Data[2] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.156, Math.Round(pair.Input.Data[3] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.012, Math.Round(pair.Input.Data[4] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.0040, Math.Round(pair.Input.Data[5] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.375, Math.Round(pair.Input.Data[6] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.562, Math.Round(pair.Input.Data[7] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.03, Math.Round(pair.Input.Data[8] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.0020, Math.Round(pair.Input.Data[9] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.57, Math.Round(pair.Input.Data[10] * 100.0) / 100.0);
+            Assert.AreEqual(0.929, Math.Round(pair.Input.Data[11] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.025, Math.Round(pair.Input.Data[12] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.0070, Math.Round(pair.Input.Data[13] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.1, Math.Round(pair.Input.Data[14] * 10.0) / 10.0);
+            Assert.AreEqual(-0.084, Math.Round(pair.Input.Data[15] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.03, Math.Round(pair.Input.Data[16] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.024, Math.Round(pair.Input.Data[17] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.008, Math.Round(pair.Input.Data[18] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.172, Math.Round(pair.Input.Data[19] * 1000.0) / 1000.0);
+
+            itr2.MoveNext();
+            pair = itr2.Current;
+            Assert.AreEqual(20, pair.Input.Count);
+            Assert.AreEqual(1, pair.Ideal.Count);
+
+            Assert.AreEqual(0.012, Math.Round(pair.Input.Data[0] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.0040, Math.Round(pair.Input.Data[1] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.375, Math.Round(pair.Input.Data[2] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.562, Math.Round(pair.Input.Data[3] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.03, Math.Round(pair.Input.Data[4] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.0020, Math.Round(pair.Input.Data[5] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.57, Math.Round(pair.Input.Data[6] * 100.0) / 100.0);
+            Assert.AreEqual(0.929, Math.Round(pair.Input.Data[7] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.025, Math.Round(pair.Input.Data[8] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.0070, Math.Round(pair.Input.Data[9] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.08, Math.Round(pair.Input.Data[10] * 100.0) / 100.0);
+            Assert.AreEqual(-0.084, Math.Round(pair.Input.Data[11] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.03, Math.Round(pair.Input.Data[12] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.024, Math.Round(pair.Input.Data[13] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.0080, Math.Round(pair.Input.Data[14] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.172, Math.Round(pair.Input.Data[15] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.014, Math.Round(pair.Input.Data[16] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.0090, Math.Round(pair.Input.Data[17] * 1000.0) / 1000.0);
+            Assert.AreEqual(-0.060, Math.Round(pair.Input.Data[18] * 1000.0) / 1000.0);
+            Assert.AreEqual(0.066, Math.Round(pair.Input.Data[19] * 1000.0) / 1000.0);
+
+        }
+    }
+}
---
+++ encog-test/encog-test/Encog/Matrix/TestBiPolarUtil.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Encog.Matrix;
+
+
+namespace encog_test.Matrix
+{
+    [TestFixture]
+    public class TestBiPolarUtil
+    {
+        [Test]
+        public void Bipolar2double()
+        {
+            // test a 1x4
+            bool[] boolData1 = { true, false, true, false };
+            double[] checkData1 = { 1, -1, 1, -1 };
+            Encog.Matrix.Matrix matrix1 = Encog.Matrix.Matrix.CreateRowMatrix(BiPolarUtil.Bipolar2double(boolData1));
+            Encog.Matrix.Matrix checkMatrix1 = Encog.Matrix.Matrix.CreateRowMatrix(checkData1);
+            Assert.IsTrue(matrix1.Equals(checkMatrix1));
+
+            // test a 2x2
+            bool[,] boolData2 = { { true, false }, { false, true } };
+            double[,] checkData2 = { { 1, -1 }, { -1, 1 } };
+            Encog.Matrix.Matrix matrix2 = new Encog.Matrix.Matrix(BiPolarUtil.Bipolar2double(boolData2));
+            Encog.Matrix.Matrix checkMatrix2 = new Encog.Matrix.Matrix(checkData2);
+            Assert.IsTrue(matrix2.Equals(checkMatrix2));
+        }
+
+        [Test]
+        public void Double2bipolar()
+        {
+            // test a 1x4
+            double[] doubleData1 = { 1, -1, 1, -1 };
+            bool[] checkData1 = { true, false, true, false };
+            bool[] result1 = BiPolarUtil.Double2bipolar(doubleData1);
+            for (int i = 0; i < checkData1.Length; i++)
+            {
+                Assert.AreEqual(checkData1[i], result1[i]);
+            }
+
+            // test a 2x2
+            double[,] doubleData2 = new double[2, 2] { { 1, -1 }, { -1, 1 } };
+            bool[,] checkData2 = new bool[2, 2] { { true, false }, { false, true } };
+            bool[,] result2 = BiPolarUtil.Double2bipolar(doubleData2);
+
+            Assert.AreEqual(result2[0, 0], checkData2[0, 0]);
+            Assert.AreEqual(result2[0, 1], checkData2[0, 1]);
+            Assert.AreEqual(result2[1, 0], checkData2[1, 0]);
+            Assert.AreEqual(result2[1, 1], checkData2[1, 1]);
+
+        }
+
+        [Test]
+        public void Binary()
+        {
+            Assert.AreEqual(0.0, BiPolarUtil.NormalizeBinary(-1));
+            Assert.AreEqual(1.0, BiPolarUtil.NormalizeBinary(2));
+            Assert.AreEqual(1.0, BiPolarUtil.ToBinary(1));
+            Assert.AreEqual(-1.0, BiPolarUtil.ToBiPolar(0));
+            Assert.AreEqual(1.0, BiPolarUtil.ToNormalizedBinary(10));
+        }
+
+    }
+}
