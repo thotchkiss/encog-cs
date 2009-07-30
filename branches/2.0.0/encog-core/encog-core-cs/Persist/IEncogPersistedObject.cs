9f17ddc1915ff47a9fce50d075e6cc62acc1ce8c
JeffHeatonDotCom <JeffHeatonDotCom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2009-07-30 12:00:26 +0000

    Branching 2.0.0

---
+++ branches/2.0.0/encog-core/encog-core-cs/Persist/IEncogPersistedObject.cs
@@ -0,0 +1,67 @@
+﻿// Encog Artificial Intelligence Framework v2.x
+// DotNet Version
+// http://www.heatonresearch.com/encog/
+// http://code.google.com/p/encog-cs/
+// 
+// Copyright 2009, Heaton Research Inc., and individual contributors.
+// See the copyright.txt in the distribution for a full listing of 
+// individual contributors.
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encog.Persist
+{
+    /// <summary>
+    /// An Encog persisted object, that can be written to XML.
+    /// </summary>
+    public interface IEncogPersistedObject : ICloneable
+    {
+        /// <summary>
+        /// The description for this object.
+        /// </summary>
+        String Description
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The name of this object.
+        /// </summary>
+        String Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Create a persistor for this object.
+        /// </summary>
+        /// <returns>A persistor for this object.</returns>
+        IPersistor CreatePersistor();
+
+        /// <summary>
+        /// Clone the object.
+        /// </summary>
+        /// <returns>A cloned version of this object.</returns>
+        new Object Clone();
+    }
+}
---
+++ branches/2.0.0/encog-core/encog-core-cs/Util/Randomize/IRandomizer.cs
@@ -0,0 +1,81 @@
+﻿// Encog Artificial Intelligence Framework v2.x
+// DotNet Version
+// http://www.heatonresearch.com/encog/
+// http://code.google.com/p/encog-cs/
+// 
+// Copyright 2009, Heaton Research Inc., and individual contributors.
+// See the copyright.txt in the distribution for a full listing of 
+// individual contributors.
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Encog.Neural.Networks;
+
+namespace Encog.Util.Randomize
+{
+    /// <summary>
+    /// Defines the interface for a class that is capable of randomizing the weights
+    /// and thresholds of a neural network.
+    /// </summary>
+    public interface IRandomizer
+    {
+        /// <summary>
+        /// Starting with the specified number, randomize it to the degree specified
+        /// by this randomizer. This could be a totally new random number, or it
+        /// could be based on the specified number.
+        /// </summary>
+        /// <param name="d">The number to randomize.</param>
+        /// <returns>A randomized number.</returns>
+        double Randomize(double d);
+
+        /// <summary>
+        /// Randomize the array based on an array, modify the array. Previous values
+        /// may be used, or they may be discarded, depending on the randomizer.
+        /// </summary>
+        /// <param name="d">An array to randomize.</param>
+        void Randomize(double[] d);
+
+        /// <summary>
+        /// Randomize the 2d array based on an array, modify the array. Previous
+        /// values may be used, or they may be discarded, depending on the
+        /// randomizer.
+        /// </summary>
+        /// <param name="d">An array to randomize.</param>
+        void Randomize(double[][] d);
+
+
+        /// <summary>
+        /// Randomize the matrix based on an array, modify the array. Previous values
+        /// may be used, or they may be discarded, depending on the randomizer.
+        /// </summary>
+        /// <param name="m">A matrix to randomize.</param>
+        void Randomize(Matrix.Matrix m);
+
+        /// <summary>
+        /// Randomize the synapses and thresholds in the basic network based on an
+        /// array, modify the array. Previous values may be used, or they may be
+        /// discarded, depending on the randomizer.
+        /// </summary>
+        /// <param name="network">A network to randomize.</param>
+        void Randomize(BasicNetwork network);
+
+    }
+
+}
