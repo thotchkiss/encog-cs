326305f63d1347a1cb5e3d89367cf084e310804c
JeffHeatonDotCom <JeffHeatonDotCom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2009-09-15 21:43:59 +0000

    Updated tests for new matrix code.

--- trunk/encog-test/encog-test/Encog/Matrix/TestBiPolarUtil.cs
+++ trunk/encog-test/encog-test/Encog/Matrix/TestBiPolarUtil.cs
@@ -22,8 +22,12 @@ namespace encog_test.TestMatrix
             Assert.IsTrue(matrix1.Equals(checkMatrix1));
 
             // test a 2x2
-            bool[,] boolData2 = { { true, false }, { false, true } };
-            double[,] checkData2 = { { 1, -1 }, { -1, 1 } };
+            bool[][] boolData2 = { 
+                new bool[2]{ true, false }, 
+                new bool[2]{ false, true } };
+            double[][] checkData2 = { 
+                new double[2] { 1, -1 }, 
+                new double[2] { -1, 1 } };
             Matrix matrix2 = new Matrix(BiPolarUtil.Bipolar2double(boolData2));
             Matrix checkMatrix2 = new Matrix(checkData2);
             Assert.IsTrue(matrix2.Equals(checkMatrix2));
@@ -42,14 +46,18 @@ namespace encog_test.TestMatrix
             }
 
             // test a 2x2
-            double[,] doubleData2 = new double[2, 2] { { 1, -1 }, { -1, 1 } };
-            bool[,] checkData2 = new bool[2, 2] { { true, false }, { false, true } };
-            bool[,] result2 = BiPolarUtil.Double2bipolar(doubleData2);
+            double[][] doubleData2 =  { 
+                new double[2] { 1, -1 },
+                new double[2] { -1, 1 } };
+            bool[][] checkData2 =  { 
+                new bool[2] { true, false }, 
+                new bool[2] { false, true } };
+            bool[][] result2 = BiPolarUtil.Double2bipolar(doubleData2);
 
-            Assert.AreEqual(result2[0, 0], checkData2[0, 0]);
-            Assert.AreEqual(result2[0, 1], checkData2[0, 1]);
-            Assert.AreEqual(result2[1, 0], checkData2[1, 0]);
-            Assert.AreEqual(result2[1, 1], checkData2[1, 1]);
+            Assert.AreEqual(result2[0][0], checkData2[0][0]);
+            Assert.AreEqual(result2[0][1], checkData2[0][1]);
+            Assert.AreEqual(result2[1][0], checkData2[1][0]);
+            Assert.AreEqual(result2[1][1], checkData2[1][1]);
 
         }
 
