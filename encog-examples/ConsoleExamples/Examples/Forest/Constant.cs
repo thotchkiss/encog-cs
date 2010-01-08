016836333b77720d389e9145c70fa013d55ed825
jeffheatondotcom <jeffheatondotcom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2010-01-08 15:52:46 +0000

    Update examples for 2.3

---
+++ encog-examples/ConsoleExamples/Examples/Forest/Constant.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encog.Examples.Forest
+{
+    /// <summary>
+    /// Modify these to suit the paths on your computer.
+    /// </summary>
+    public class Constant
+    {
+        /// <summary>
+        /// The base directory that all of the data for this example is stored in.
+        /// </summary>
+        public const String BASE_DIRECTORY = "c:\\data\\";
+
+        /// <summary>
+        /// The source data file from which all others are built.  This file can
+        /// be downloaded from:
+        /// 
+        /// http://kdd.ics.uci.edu/databases/covertype/covertype.html
+        /// </summary>
+        public const String COVER_TYPE_FILE = BASE_DIRECTORY + "covtype.data";
+
+        /// <summary>
+        /// 75% of the data will be moved into this file to be used as training data.  The 
+        /// data is still in "raw form" in this file.
+        /// </summary>
+        public const String TRAINING_FILE = BASE_DIRECTORY + "training.csv";
+
+        /// <summary>
+        /// 25% of the data will be moved into this file to be used as evaluation data.  The 
+        /// data is still in "raw form" in this file.
+        /// </summary>
+        public const String EVALUATE_FILE = BASE_DIRECTORY + "evaluate.csv";
+
+        /// <summary>
+        /// We will limit the number of samples per "tree type" to 3000, this causes the data
+        /// to be more balanced and will not allow one tree type to over-fit the network.
+        /// The training file is narrowed and placed into this file in "raw form".
+        /// </summary>
+        public const String BALANCE_FILE = BASE_DIRECTORY + "balance.csv";
+
+        /// <summary>
+        /// The training file is normalized and placed into this file.
+        /// </summary>
+        public const String NORMALIZED_FILE = BASE_DIRECTORY + "normalized.csv";
+
+        /// <summary>
+        /// CSV files are slow to parse with because the text must be converted into numbers.
+        /// The balanced file will be converted to a binary file to be used for training.
+        /// </summary>
+        public const String BINARY_FILE = BASE_DIRECTORY + "normalized.bin";
+
+        /// <summary>
+        /// The trained network and normalizer will be saved into an Encog EG file.
+        /// </summary>
+        public const String TRAINED_NETWORK_FILE = BASE_DIRECTORY + "forest.eg";
+
+        /// <summary>
+        /// The name of the network inside of the EG file.
+        /// </summary>
+        public const String TRAINED_NETWORK_NAME = "forest-network";
+
+        /// <summary>
+        /// The name of the normalization object inside of the EG file.
+        /// </summary>
+        public const String NORMALIZATION_NAME = "forest-norm";
+
+        /// <summary>
+        /// How many minutes to train for (console mode only)
+        /// </summary>
+        public const int TRAINING_MINUTES = 10;
+
+        /// <summary>
+        /// How many hidden neurons to use.
+        /// </summary>
+        public const int HIDDEN_COUNT = 100;
+    }
+}
---
+++ encog-examples/ConsoleExamples/Examples/Forest/TrainNetwork.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Encog.Neural.Networks;
+using Encog.Neural.NeuralData;
+using Encog.Neural.Activation;
+using Encog.Neural.Networks.Layers;
+using Encog.Neural.Networks.Logic;
+using Encog.Persist;
+using Encog.Normalize;
+using Encog.Neural.Data.Buffer;
+using Encog.Util.Simple;
+using System.IO;
+
+namespace Encog.Examples.Forest
+{
+    public class TrainNetwork
+    {
+        private IExampleInterface app;
+
+        public TrainNetwork(IExampleInterface app)
+        {
+            this.app = app;
+        }
+
+        public BasicNetwork GenerateNetwork(INeuralDataSet trainingSet)
+        {
+            BasicNetwork network = new BasicNetwork();
+            network.AddLayer(new BasicLayer(new ActivationSigmoid(), true, trainingSet.InputSize));
+            network.AddLayer(new BasicLayer(new ActivationSigmoid(), true, Constant.HIDDEN_COUNT));
+            network.AddLayer(new BasicLayer(new ActivationSigmoid(), true, trainingSet.IdealSize));
+            network.Logic = new FeedforwardLogic();
+            network.Structure.FinalizeStructure();
+            network.Reset();
+            return network;
+        }
+
+        public void Train(bool useGUI)
+        {
+            app.WriteLine("Converting training file to binary");
+            EncogPersistedCollection encog = new EncogPersistedCollection(Constant.TRAINED_NETWORK_FILE, FileMode.Open);
+            DataNormalization norm = (DataNormalization)encog.Find(Constant.NORMALIZATION_NAME);
+
+            EncogUtility.convertCSV2Binary(Constant.NORMALIZED_FILE, Constant.BINARY_FILE, norm.GetNetworkInputLayerSize(), norm.GetNetworkOutputLayerSize(), false);
+            BufferedNeuralDataSet trainingSet = new BufferedNeuralDataSet(Constant.BINARY_FILE);
+
+            BasicNetwork network = (BasicNetwork)encog.Find(Constant.TRAINED_NETWORK_NAME);
+            if (network == null)
+                network = EncogUtility.simpleFeedForward(norm.GetNetworkInputLayerSize(), Constant.HIDDEN_COUNT, 0, norm.GetNetworkOutputLayerSize(), false);
+
+            if (useGUI)
+            {
+                EncogUtility.trainDialog(network, trainingSet);
+            }
+            else
+            {
+                EncogUtility.trainConsole(network, trainingSet, Constant.TRAINING_MINUTES);
+            }
+
+            app.WriteLine("Training complete, saving network...");
+            encog.Add(Constant.TRAINED_NETWORK_NAME, network);
+        }
+
+    }
+}
