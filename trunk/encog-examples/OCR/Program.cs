acb9d1e65a4e263423e336a3cc2dfaef405225c3
JeffHeatonDotCom <JeffHeatonDotCom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2010-02-15 23:10:35 +0000

    Update headers for 2.3 for examples

--- trunk/encog-examples/OCR/Program.cs
+++ trunk/encog-examples/OCR/Program.cs
@@ -1,13 +1,31 @@
-﻿// Introduction to Neural Networks for C#, 2nd Edition
-// Copyright 2008 by Heaton Research, Inc. 
-// http://www.heatonresearch.com/online/introduction-neural-networks-cs-edition-2
-//         
-// ISBN13: 978-1-60439-009-4  	 
-// ISBN:   1-60439-009-3
-//   
-// This class is released under the:
-// GNU Lesser General Public License (LGPL)
-// http://www.gnu.org/copyleft/lesser.html
+// Encog(tm) Artificial Intelligence Framework v2.3: C# Examples
+// .Net Version
+// http://www.heatonresearch.com/encog/
+// http://code.google.com/p/encog-java/
+// 
+// Copyright 2008-2010 by Heaton Research Inc.
+// 
+// Released under the LGPL.
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+// 
+// Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+// For information on Heaton Research trademarks, visit:
+// 
+// http://www.heatonresearch.com/copyright.html
 
 using System;
 using System.Collections.Generic;
