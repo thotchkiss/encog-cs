e366d894ad143d7a457fda0e35f6868db260e50a
jeffheatondotcom <jeffheatondotcom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2010-03-20 02:34:27 +0000

    Updated examples for new genetic code

--- encog-examples/ConsoleExamples/Examples/GeneticTSP/GeneticSolveTSP.cs
+++ encog-examples/ConsoleExamples/Examples/GeneticTSP/GeneticSolveTSP.cs
@@ -33,11 +33,20 @@ using System.Linq;
 using System.Text;
 using Encog.Examples.Util;
 using ConsoleExamples.Examples;
+using Encog.Solve.Genetic.Genes;
+using Encog.Solve.Genetic;
+using Encog.MathUtil;
+using Encog.Solve.Genetic.Genome;
+using Encog.Solve.Genetic.Population;
+using Encog.Solve.Genetic.Crossover;
+using Encog.Solve.Genetic.Mutate;
 
 namespace Encog.Examples.GeneticTSP
 {
-    public class GeneticSolveTSP: IExample
+    public class GeneticSolveTSP : IExample
     {
+        private IExampleInterface app;
+
         public static ExampleInfo Info
         {
             get
@@ -52,6 +61,7 @@ namespace Encog.Examples.GeneticTSP
         }
 
 
+
         public const int CITIES = 50;
         public const int POPULATION_SIZE = 1000;
         public const double MUTATION_PERCENT = 0.1;
@@ -60,85 +70,63 @@ namespace Encog.Examples.GeneticTSP
         public const int CUT_LENGTH = CITIES / 5;
         public const int MAP_SIZE = 256;
         public const int MAX_SAME_SOLUTION = 25;
-        private IExampleInterface app;
 
-        /// <summary>
-        /// The genetic algorithm to use.
-        /// </summary>
-        private TSPGeneticAlgorithm genetic;
-
-        /// <summary>
-        /// The cities to use.
-        /// </summary>
+        private GeneticAlgorithm genetic;
         private City[] cities;
 
-        /// <summary>
-        /// Place the cities in random locations.
-        /// </summary>
-        private void InitCities()
+        /**
+         * Place the cities in random locations.
+         */
+        private void initCities()
         {
-            Random rand = new Random();
-
             cities = new City[CITIES];
             for (int i = 0; i < cities.Length; i++)
             {
-                int xPos = (int)(rand.NextDouble() * MAP_SIZE);
-                int yPos = (int)(rand.NextDouble() * MAP_SIZE);
+                int xPos = (int)(ThreadSafeRandom.NextDouble() * MAP_SIZE);
+                int yPos = (int)(ThreadSafeRandom.NextDouble() * MAP_SIZE);
 
                 cities[i] = new City(xPos, yPos);
             }
         }
 
-        /// <summary>
-        /// Create an initial path of cities.
-        /// </summary>
-        private void InitPath()
+        private void initPopulation(GeneticAlgorithm ga)
         {
-            Random rand = new Random();
-            bool[] taken = new bool[this.cities.Length];
-            int[] path = new int[this.cities.Length];
+            ICalculateGenomeScore score = new TSPScore(cities);
+            ga.CalculateScore = score;
+            IPopulation population = new BasicPopulation(POPULATION_SIZE);
+            ga.Population = population;
 
-            for (int i = 0; i < path.Length; i++)
+            for (int i = 0; i < POPULATION_SIZE; i++)
             {
-                taken[i] = false;
+
+                TSPGenome genome = new TSPGenome(ga, cities);
+                ga.Population.Genomes.Add(genome);
+                ga.PerformScoreCalculation(genome);
             }
-            for (int i = 0; i < path.Length - 1; i++)
-            {
-                int icandidate;
-                do
-                {
-                    icandidate = (int)(rand.NextDouble() * path.Length);
-                } while (taken[icandidate]);
-                path[i] = icandidate;
-                taken[icandidate] = true;
-                if (i == path.Length - 2)
-                {
-                    icandidate = 0;
-                    while (taken[icandidate])
-                    {
-                        icandidate++;
-                    }
-                    path[i + 1] = icandidate;
-                }
-            }
+            population.Sort();
         }
 
-        /// <summary>
-        /// Display the cities in the final path.
-        /// </summary>
-        public void DisplaySolution()
+
+        /**
+         * Display the cities in the final path.
+         */
+        public void displaySolution()
         {
-            int[] path = genetic.Chromosomes[0].Genes;
-            for (int i = 0; i < path.Length; i++)
+
+            bool first = true;
+
+            foreach (IGene gene in genetic.Population.GetBest().Chromosomes[0].Genes)
             {
-                if (i != 0)
-                {
-                    app.Write(">");
-                }
-                app.Write("" + path[i]);
+                if (!first)
+                    Console.Write(">");
+                Console.Write("" + ((IntegerGene)gene).Value);
+                first = false;
             }
-            app.WriteLine("");
+
+            Console.WriteLine("");
         }
+
+
 
         /// <summary>
         /// Setup and solve the TSP.
@@ -146,21 +134,19 @@ namespace Encog.Examples.GeneticTSP
         public void Execute(IExampleInterface app)
         {
             this.app = app;
+
             StringBuilder builder = new StringBuilder();
 
-            InitCities();
+            initCities();
 
-            genetic = new TSPGeneticAlgorithm(
-                    cities,
-                    POPULATION_SIZE,
-                    MUTATION_PERCENT,
-                    PERCENT_TO_MATE,
-                    MATING_POPULATION_PERCENT,
-                    CUT_LENGTH);
+            genetic = new GeneticAlgorithm();
 
-
-
-            InitPath();
+            initPopulation(genetic);
+            genetic.MutationPercent = MUTATION_PERCENT;
+            genetic.PercentToMate = PERCENT_TO_MATE;
+            genetic.MatingPopulation = MATING_POPULATION_PERCENT;
+            genetic.Crossover = new SpliceNoRepeat(CITIES / 3);
+            genetic.Mutate = new MutateShuffle();
 
             int sameSolutionCount = 0;
             int iteration = 1;
@@ -170,7 +156,7 @@ namespace Encog.Examples.GeneticTSP
             {
                 genetic.Iteration();
 
-                double thisSolution = genetic.Chromosomes[0].Score;
+                double thisSolution = genetic.Population.GetBest().Score;
 
                 builder.Length = 0;
                 builder.Append("Iteration: ");
@@ -178,7 +164,7 @@ namespace Encog.Examples.GeneticTSP
                 builder.Append(", Best Path Length = ");
                 builder.Append(thisSolution);
 
-                app.WriteLine(builder.ToString());
+                Console.WriteLine(builder.ToString());
 
                 if (Math.Abs(lastSolution - thisSolution) < 1.0)
                 {
@@ -192,8 +178,8 @@ namespace Encog.Examples.GeneticTSP
                 lastSolution = thisSolution;
             }
 
-            app.WriteLine("Good solution found:");
-            DisplaySolution();
+            Console.WriteLine("Good solution found:");
+            displaySolution();
 
         }
     }
