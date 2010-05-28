30a36b59b8edb884fd6e1733cc63f33f230dee54
jeffheatondotcom <jeffheatondotcom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2010-05-28 12:52:36 +0000

    Done with tune example, for now.

--- encog-examples/TuneEncogOpenCL/EncogTuneForm.cs
+++ encog-examples/TuneEncogOpenCL/EncogTuneForm.cs
@@ -133,25 +133,11 @@ namespace TuneEncogOpenCL
             }
         }
 
-        private void btnAutoTune_Click(object sender, EventArgs e)
-        {
-            if (!CollectDeviceChecks())
-                return;
-
-            btnAutoTune.Enabled = false;
-            btnBenchmark.Enabled = false;
-            statusBar.Text = "Running autotune...";
-
-            Thread thread = new Thread(new ThreadStart(this.AutoTuneProc));
-            thread.Start();
-        }
-
         private void btnBenchmark_Click(object sender, EventArgs e)
         {
             if (!CollectDeviceChecks())
                 return;
 
-            btnAutoTune.Enabled = false;
             btnBenchmark.Enabled = false;
 
             Thread thread = new Thread(new ThreadStart(this.BenchmarkProc));
@@ -188,7 +174,7 @@ namespace TuneEncogOpenCL
                 //
                 network.Reset();
                 ResilientPropagation train = new ResilientPropagation(network, training);
-                train.NumThreads = -1;
+                train.NumThreads = 0;
                 train.Iteration();
                 Stopwatch stopwatch = new Stopwatch();
                 stopwatch.Start();
@@ -239,7 +225,6 @@ namespace TuneEncogOpenCL
                 //
                 this.Invoke((MethodInvoker)delegate { this.textCalcCLRatio.Text = ratio; });
                 this.Invoke((MethodInvoker)delegate { statusBarText.Text = "Ready."; });
-                this.Invoke((MethodInvoker)delegate { btnAutoTune.Enabled = true; });
                 this.Invoke((MethodInvoker)delegate { btnBenchmark.Enabled = true; });
             }
             catch (Exception ex)
@@ -248,139 +233,9 @@ namespace TuneEncogOpenCL
             }
         }
 
-        public void AutoTuneProc()
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            statusBar.Invoke((MethodInvoker)delegate { statusBarText.Text = "Running autotune..."; });
-
-            int trainingSize = int.Parse(textTrainingSize.Text);
-            int inputSize = int.Parse(textInputNeurons.Text);
-            int outputSize = int.Parse(textOutputNeurons.Text);
-            int hiddenSize = int.Parse(textHiddenNeurons.Text);
-
-            INeuralDataSet training = RandomTrainingFactory.Generate(trainingSize, inputSize, outputSize, -1, 1);
-            BasicNetwork network = EncogUtility.SimpleFeedForward(
-                training.InputSize, hiddenSize, 0, training.IdealSize, true);
-            network.Reset();
-
-            long bestTicks = long.MaxValue;
-            int bestThreads = 0;
-
-            Encog.Encog.Instance.CL.CLThreads = 25;
-            Encog.Encog.Instance.CL.CLWorkloadSize = 25;
-            ResilientPropagation warmUp = new ResilientPropagation(network, training);
-            warmUp.NumThreads = -1;
-            for (int i = 1; i <= 10; i++)
-            {
-                this.Invoke((MethodInvoker)delegate { statusBarText.Text = "Autotune...Warmup..." + i + "/10"; });
-                for(int j=0;j<50;j++)
-                    warmUp.Iteration();
-            }
-
-            int sameCount = 0;
-
-            for (int i = 2; i < 500; i++)
-            {
-                try
-                {
-                    Encog.Encog.Instance.CL.CLThreads = i;
-                    Encog.Encog.Instance.CL.CLWorkloadSize = i;
-
-                    ResilientPropagation train = new ResilientPropagation(network, training);
-                    train.NumThreads = -1;
-                    train.Iteration();
-                    Stopwatch stopwatch = new Stopwatch();
-                    stopwatch.Start();
-                    for (int j = 0; j < 25; j++)
-                        train.Iteration();
-                    stopwatch.Stop();
-
-                    if (stopwatch.ElapsedTicks < bestTicks)
-                    {
-                        bestTicks = stopwatch.ElapsedTicks;
-                        bestThreads = i;
-                        sameCount = 0;
-                    }
-                    else
-                    {
-                        sameCount++;
-                        if (sameCount >= 50)
-                            break;
-                    }
-
-                    this.Invoke((MethodInvoker)delegate
-                    {
-                        statusBarText.Text = "Autotune...Threads " + i + "/500 :  "
-                            + Format.FormatInteger((int)stopwatch.ElapsedTicks) + "ticks, best= threads:" + bestThreads +
-                            ", ticks:" + Format.FormatInteger((int)bestTicks);
-                    });
-                }
-                catch (Exception)
-                {
-                    // too many threads
-                    break;
-                }
-            }
-
-            this.Invoke((MethodInvoker)delegate { this.textCLThreadCount.Text = "" + bestThreads; });
-            Encog.Encog.Instance.CL.CLThreads = bestThreads;
-
-            //
-            // Now find the workgroup size
-            //
-            int bestWorkgroup = 0;
-            sameCount = 0;
-            bestTicks = int.MaxValue;
-
-            for (int i = 1; i < bestThreads; i++)
-            {
-                try
-                {
-                    Encog.Encog.Instance.CL.CLThreads = bestThreads;
-                    Encog.Encog.Instance.CL.CLWorkloadSize = i;
-
-                    ResilientPropagation train = new ResilientPropagation(network, training);
-                    train.NumThreads = -1;
-                    train.Iteration();
-                    Stopwatch stopwatch = new Stopwatch();
-                    stopwatch.Start();
-                    for (int j = 0; j < 25; j++)
-                        train.Iteration();
-                    stopwatch.Stop();
-
-                    if (stopwatch.ElapsedTicks < bestTicks)
-                    {
-                        bestTicks = stopwatch.ElapsedTicks;
-                        bestWorkgroup = i;
-                        sameCount = 0;
-                    }
-                    else
-                    {
-                        sameCount++;
-                        if (sameCount >= 50)
-                            break;
-                    }
-
-                    this.Invoke((MethodInvoker)delegate
-                    {
-                        statusBarText.Text = "Autotune...Workgroup " + i + "/" + bestThreads + " :  "
-                            + Format.FormatInteger((int)stopwatch.ElapsedTicks) + "ticks, best= workgroup:" + bestWorkgroup +
-                            ", ticks:" + Format.FormatInteger((int)bestTicks);
-                    });
-                }
-                catch (Exception)
-                {
-                    // too many threads
-                    break;
-                }
-            }
-
-            this.Invoke((MethodInvoker)delegate { this.textWorkgroupSize.Text = "" + bestWorkgroup; });
-
-
-            //this.Invoke((MethodInvoker)delegate { this.textCalcCLRatio.Text = ratio; });
-            this.Invoke((MethodInvoker)delegate { statusBarText.Text = "Ready."; });
-            this.Invoke((MethodInvoker)delegate { btnAutoTune.Enabled = true; });
-            this.Invoke((MethodInvoker)delegate { btnBenchmark.Enabled = true; });
+            System.Diagnostics.Process.Start("http://www.heatonresearch.com/encog/tune1.html");
         }
 
     }
