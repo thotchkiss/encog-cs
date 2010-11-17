001610398c3a51a40f80720202119aaa1ed3bfc0
jeffheatondotcom <jeffheatondotcom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2010-11-17 02:08:52 +0000

    Branching 2.5.0

---
+++ branches/2.5.0/encog-core/encog-test/Encog/Data/Market/TestYahooLoader.cs
@@ -0,0 +1,60 @@
+// Encog(tm) Artificial Intelligence Framework v2.5
+// .Net Version
+// http://www.heatonresearch.com/encog/
+// http://code.google.com/p/encog-java/
+// 
+// Copyright 2008-2010 by Heaton Research Inc.
+// 
+// Released under the LGPL.
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+// 
+// Encog and Heaton Research are Trademarks of Heaton Research, Inc.
+// For information on Heaton Research trademarks, visit:
+// 
+// http://www.heatonresearch.com/copyright.html
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Encog.Neural.NeuralData.Market.Loader;
+using Encog.Neural.NeuralData.Market;
+
+namespace encog_test.Data.Market
+{
+    [TestFixture]
+    public class TestYahooLoader
+    {
+        [Test]
+        public void Loader()
+        {
+            IMarketLoader loader = new YahooFinanceLoader();
+            DateTime from = new DateTime(2008, 8, 4);
+            DateTime to = new DateTime(2008, 8, 5);
+            ICollection<LoadedMarketData> list = loader.Load(new TickerSymbol("aapl"), null, from, to);
+
+            IEnumerator<LoadedMarketData> itr = list.GetEnumerator();
+            itr.MoveNext();
+            LoadedMarketData data = itr.Current;
+            Assert.AreEqual(160, (int)data.GetData(MarketDataType.CLOSE));
+            itr.MoveNext();
+            data = itr.Current;
+            Assert.AreEqual(153, (int)data.GetData(MarketDataType.CLOSE));
+        }
+    }
+}
