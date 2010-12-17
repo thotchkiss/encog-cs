1b17061f47b336a824932593247120cbc8d88acf
jeffheatondotcom <jeffheatondotcom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2010-12-17 11:41:29 +0000

    Adding separate quant example

---
+++ encog-cs-quant/encog-cs-quant-example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Encog.App.Quant;
+
+namespace encog_cs_quant_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EncogQuant quant = new EncogQuant();
+            quant.Data.Add(new FileData(FileData.OPEN, true, 3));
+            quant.Data.Add(new FileData(FileData.HIGH, true, 4));
+            quant.Data.Add(new FileData(FileData.LOW, true, 5));
+            quant.Data.Add(new FileData(FileData.CLOSE, true, 6));
+            quant.Data.Add(new FileData(FileData.VOLUME, true, 7));
+            quant.Data.Add(new MovingAverage(20, true));
+            quant.Process("d:\\data\\EURUSD.txt","d:\\data\\EURUSD2.txt");
+        }
+    }
+}
---
+++ encog-cs-quant/encog-cs-quant/Field.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encog.App.Quant
+{
+    public interface Field
+    {
+        String Name { get; }
+        bool Output { get; }
+        int Index { get; }
+    }
+}
