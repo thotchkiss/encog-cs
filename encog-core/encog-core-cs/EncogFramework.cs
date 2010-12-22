a32fe1489bd25ec306840e56f63df59deae9741b
jeffheatondotcom <jeffheatondotcom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2010-12-22 13:23:38 +0000

    2.5.3: Performance improvements.

--- encog-core/encog-core-cs/EncogFramework.cs
+++ encog-core/encog-core-cs/EncogFramework.cs
@@ -52,7 +52,7 @@ namespace Encog
 		/// <summary>
         /// The current engog version, this should be read from the properties.
 		/// </summary>
-	    public static String VERSION = "2.5.2";
+	    public static String VERSION = "2.5.3";
 
         /// <summary>
         /// The platform.
