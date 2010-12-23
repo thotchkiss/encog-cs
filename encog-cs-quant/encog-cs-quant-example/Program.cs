88514209a2d9e95d59259a8a21e0b061c9b0aa52
jeffheatondotcom <jeffheatondotcom@0d3f3ea6-ac51-0410-a1de-edb5cd2a854a>
2010-12-23 12:37:51 +0000

    Quant example

--- encog-cs-quant/encog-cs-quant-example/Program.cs
+++ encog-cs-quant/encog-cs-quant-example/Program.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Encog.App.Quant;
+using Encog.App.Quant.Indicators;
+using Encog.App.Quant.Indicators.Predictive;
 
 namespace encog_cs_quant_example
 {
     class Program
     {
-        static void Main(string[] args)
+        static void CalculateCurrency()
         {
             EncogQuant quant = new EncogQuant();
             quant.Data.Add(new FileData(FileData.OPEN, true, 3));
@@ -17,7 +19,40 @@ namespace encog_cs_quant_example
             quant.Data.Add(new FileData(FileData.CLOSE, true, 6));
             quant.Data.Add(new FileData(FileData.VOLUME, true, 7));
             quant.Data.Add(new MovingAverage(20, true));
-            quant.Process("d:\\data\\EURUSD.txt","d:\\data\\EURUSD2.txt");
+            quant.Data.Add(new RelativeStrengthIndex(14, true));
+            quant.Process("d:\\data\\EURUSD.txt", "d:\\data\\EURUSD2.txt");
+        }
+
+        static void CalculateStockEOD()
+        {
+            EncogQuant quant = new EncogQuant();
+            quant.Data.Add(new FileData(FileData.OPEN, true, 1));
+            quant.Data.Add(new FileData(FileData.HIGH, true, 2));
+            quant.Data.Add(new FileData(FileData.LOW, true, 3));
+            quant.Data.Add(new FileData(FileData.CLOSE, true, 4));
+            quant.Data.Add(new FileData(FileData.VOLUME, true, 5));
+            quant.Data.Add(new MovingAverage(20, true));
+            quant.Data.Add(new RelativeStrengthIndex(14, true));
+            quant.Data.Add(new StochasticOscillator(14, true));
+            quant.Data.Add(new BestClose(3,true));
+            quant.Process("d:\\data\\ge.csv", "d:\\data\\ge2.csv");
+        }
+
+        static void CalculateTest()
+        {
+            EncogQuant quant = new EncogQuant();
+            quant.Headers = false;
+            quant.Data.Add(new FileData(FileData.CLOSE, true, 0));
+            quant.Data.Add(new MovingAverage(20, true));
+            quant.Data.Add(new RelativeStrengthIndex(14, true));            
+            quant.Process("d:\\data\\test.txt", "d:\\data\\test2.txt");
+        }
+
+        static void Main(string[] args)
+        {
+            //CalculateCurrency();
+            CalculateStockEOD();
+            //CalculateTest();
         }
     }
 }
